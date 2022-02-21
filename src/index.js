c84e60a53d2007153d55789d596afb03aed924cb
Serhii Plis <serzh2577@gmail.com>
2022-02-21 13:52:47 +0200

    adds markup and css styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-import './sass/main.scss';
+import Notiflix from 'notiflix';
+import Axios from 'axios';
+
+const searchForm = document.querySelector('#search-form');
+
+searchForm.addEventListener('submit', onSearchBtn);
+searchForm.addEventListener('input', onSearchInput);
+
+function onSearchBtn(e) {
+  e.preventDefault();
+  console.log(e.target.value);
+}
+
+function onSearchInput(e) {
+  console.log(e.target.value);
+}
